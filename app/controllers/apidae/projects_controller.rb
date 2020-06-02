bf1c7163a32d4ccee2883541644576b7814f3d6b
Jean-Baptiste Vilain <jbvilain@gmail.com>
2020-06-03 00:23:45 +0200

    fixes

--- app/controllers/apidae/projects_controller.rb
+++ app/controllers/apidae/projects_controller.rb
@@ -24,7 +24,7 @@ module Apidae
       else
         @project = Project.new(project_params)
         if @project.save
-          referrer = (session.delete(:referrer) || projects_url)
+          referrer = params[:redirect_to] || session.delete(:referrer) || projects_url
           redirect_to (referrer + "?apidae_project_id=#{@project.id}"), notice: 'Le projet a bien été créé'
         else
           flash.now[:alert] = "Une erreur s'est produite lors la création du projet"
@@ -54,7 +54,7 @@ module Apidae
 
     def update_project
       if @project.update(project_params)
-        referrer = session.delete(:referrer)
+        referrer = params[:redirect_to] || session.delete(:referrer)
         redirect_to (referrer + "?apidae_project_id=#{@project.id}"), notice: 'Le projet a bien été mis à jour'
       else
         flash.now[:alert] = "Une erreur s'est produite lors la mise à jour du projet"
