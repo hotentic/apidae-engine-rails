62a0b16671cf06348f2feacdf1f937291de519de
Jean-Baptiste Vilain <jbvilain@gmail.com>
2021-07-08 18:30:14 +0200

    1.3.5 - fix obj deletion during import when used by another project - report from 0.9

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_124205) do
+ActiveRecord::Schema.define(version: 2021_06_07_214647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,8 +92,10 @@ ActiveRecord::Schema.define(version: 2020_05_22_124205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.jsonb "meta_data"
+    t.boolean "is_active"
     t.index ["apidae_id"], name: "index_apidae_references_on_apidae_id"
     t.index ["apidae_type"], name: "index_apidae_references_on_apidae_type"
+    t.index ["is_active"], name: "index_apidae_references_on_is_active"
   end
 
   create_table "apidae_selection_objects", force: :cascade do |t|
@@ -110,6 +112,13 @@ ActiveRecord::Schema.define(version: 2020_05_22_124205) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "apidae_project_id"
+  end
+
+  create_table "apidae_territories", force: :cascade do |t|
+    t.integer "apidae_id"
+    t.string "name"
+    t.integer "apidae_type"
+    t.index ["apidae_id"], name: "index_apidae_territories_on_apidae_id"
   end
 
   create_table "apidae_towns", id: :serial, force: :cascade do |t|
