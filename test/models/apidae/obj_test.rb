d7172577eb9e404ef1b4fda81307cc157818d85e
Jean-Baptiste Vilain <jbvilain@gmail.com>
2022-03-29 15:47:15 +0200

    Updated accessible-autocomplete dep

--- test/models/apidae/obj_test.rb
+++ test/models/apidae/obj_test.rb
@@ -2,8 +2,15 @@ require 'test_helper'
 
 module Apidae
   class ObjTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+
+    test "populate empty booking desc" do
+      objects_json = File.read('test/data/hot_fr_en.json')
+      obj_data = JSON.parse(objects_json, symbolize_names: true).first
+
+      obj = Apidae::Obj.new
+      Apidae::Obj.populate_fields(obj, obj_data, [Apidae::LOCALE_FR])
+
+      assert_nil obj.booking_desc
+    end
   end
 end
