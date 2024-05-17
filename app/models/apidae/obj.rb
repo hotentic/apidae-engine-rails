a1fe50678a5c5954dbc8fed08f67a915a90b5df7
Jean-Baptiste Vilain <jbvilain@gmail.com>
2024-05-17 09:04:53 +0200

    0.9.35 - added support for prestations.nombrePersonnesMinimum field

--- app/models/apidae/obj.rb
+++ app/models/apidae/obj.rb
@@ -382,7 +382,7 @@ module Apidae
           categories: lists_ids(data_hash[:categories], data_hash[:typesDetailles], data_hash[:activiteCategories]),
           themes: lists_ids(data_hash[:themes]),
           capacity: (data_hash[:capacite] || {})
-                        .merge(presta_hash ? {group_min: presta_hash[:tailleGroupeMin], group_max: presta_hash[:tailleGroupeMax],
+                        .merge(presta_hash ? {group_min: (presta_hash[:nombrePersonnesMinimum] || presta_hash[:tailleGroupeMin]), group_max: presta_hash[:tailleGroupeMax],
                                               age_min: presta_hash[:ageMin], age_max: presta_hash[:ageMax]} : {}),
           classification: nodes_ids(data_hash[:classement], data_hash[:classementPrefectoral], data_hash[:classification]) +
                                     lists_ids(data_hash[:classementsGuides]) + lists_ids(data_hash[:classements]),
