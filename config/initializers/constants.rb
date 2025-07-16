06a605bc7359526dd9f39804a268bb4f640ec4dc
Jean-Baptiste Vilain <jbvilain@gmail.com>
2025-07-16 18:03:22 +0200

    Apidae 1.4.18 - fixed accessibility_desc fields

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -25,10 +25,10 @@ module Apidae
   ALL_VERSIONS = [STANDARD_VERSION, WINTER_VERSION, SUMMER_VERSION, CHALLENGED_VERSION, BUSINESS_VERSION,
                   GROUPS_VERSION, ACTIVITIES_VERSION]
 
-  LOCALIZED_FIELDS = [:title, :short_desc, :long_desc, :pictures, :attachments, :accessibility_desc,
+  LOCALIZED_FIELDS = [:title, :short_desc, :long_desc, :pictures, :attachments,
                       :openings_desc, :rates_desc, :includes, :excludes, :extra, :booking_desc]
-  # Todo : finish implem of localized accessibility fields
-  ALL_FIELDS = LOCALIZED_FIELDS + [:theme_desc, :private_desc, :capacity, :telephone, :email, :website,
+
+  ALL_FIELDS = LOCALIZED_FIELDS + [:theme_desc, :private_desc, :accessibility_desc, :capacity, :telephone, :email, :website,
                                    :google, :facebook, :twitter, :yelp, :trip_advisor, :fax, :mobile_website, :shorty_url, :instagram,
                                    :openings, :openings_extra, :rates, :services, :payment_methods, :categories, :themes,
                                    :labels, :chains, :classification, :challenged, :environments, :languages, :products,
